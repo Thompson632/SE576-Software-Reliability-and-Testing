7aad808b423319c463740a4579da3c231378aea5
Robert Thompson <robertdavidthompson632@gmail.com>
2021-02-20 13:46:53 -0500

    Initial commit for Bishop validation and unit test.
    Unit test coverage is only 50% due to testing with a default board.

--- src/test/java/com/thompson/apps/chess/pieces/TestBishop.java
+++ src/test/java/com/thompson/apps/chess/pieces/TestBishop.java
@@ -1,24 +1,114 @@
 package com.thompson.apps.chess.pieces;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import com.thompson.apps.chess.board.Cell;
+import com.thompson.apps.chess.board.ChessBoard;
+import com.thompson.apps.chess.board.Tile;
+
 public class TestBishop {
+	ChessBoard board = null;
+
+	AbstractPiece piece = null;
+
+	List<Cell> moves = null;
+
+	List<Tile> tileActualMoves = null;
+	List<Tile> tileExpectedMoves = null;
 
 	@Before
 	public void setUp() throws Exception {
+		board = new ChessBoard();
+		board.setDefaultBoard();
+
+		tileActualMoves = new ArrayList<Tile>();
+		tileExpectedMoves = new ArrayList<Tile>();
 	}
 
 	@After
 	public void tearDown() throws Exception {
+		board = null;
+
+		tileActualMoves = null;
+		tileExpectedMoves = null;
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
+	public void testDefaultBishop_WhitePieces() {
+		// First Bishop
+		piece = new Bishop(true, 0, 2);
 
+		// Assert Moves
+		moves = piece.getValidMoves(board.getChessBoard());
+		assertEquals(0, moves.size());
+
+		// Assert Piece Enum
+		assertEquals(PieceEnum.BISHOP, piece.getPieceEnum());
+
+		// Assert Color
+		assertTrue(piece.isWhite());
+
+		// Assert toString
+		assertEquals("B", piece.toString());
+
+		// Second Bishop
+		piece = new Bishop(true, 0, 5);
+
+		// Assert Moves
+		moves = piece.getValidMoves(board.getChessBoard());
+		assertEquals(0, moves.size());
+
+		// Assert Piece Enum
+		assertEquals(PieceEnum.BISHOP, piece.getPieceEnum());
+
+		// Assert Color
+		assertTrue(piece.isWhite());
+
+		// Assert toString
+		assertEquals("B", piece.toString());
+	}
+	
+	@Test
+	public void testDefaultBishop_BlackPieces() {
+		// First Bishop
+		piece = new Bishop(false, 7, 2);
+
+		// Assert Moves
+		moves = piece.getValidMoves(board.getChessBoard());
+		assertEquals(0, moves.size());
+
+		// Assert Piece Enum
+		assertEquals(PieceEnum.BISHOP, piece.getPieceEnum());
+
+		// Assert Color
+		assertFalse(piece.isWhite());
+
+		// Assert toString
+		assertEquals("b", piece.toString());
+
+		// Second Bishop
+		piece = new Bishop(false, 7, 5);
+
+		// Assert Moves
+		moves = piece.getValidMoves(board.getChessBoard());
+		assertEquals(0, moves.size());
+
+		// Assert Piece Enum
+		assertEquals(PieceEnum.BISHOP, piece.getPieceEnum());
+
+		// Assert Color
+		assertFalse(piece.isWhite());
+
+		// Assert toString
+		assertEquals("b", piece.toString());
+	}
 }
