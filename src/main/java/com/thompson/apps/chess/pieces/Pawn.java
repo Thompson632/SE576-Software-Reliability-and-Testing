cc275d4cddfb0c7defeba2b541a62f94666dbcc2
Robert Thompson <robertdavidthompson632@gmail.com>
2021-02-20 18:16:59 -0500

    Pawn Validation and Unit Test Coverage Complete.
    All piece validation has been completed. Need to handle user input for custom chess game piece validation."

--- src/main/java/com/thompson/apps/chess/pieces/Pawn.java
+++ src/main/java/com/thompson/apps/chess/pieces/Pawn.java
@@ -12,13 +12,121 @@ public class Pawn extends AbstractPiece {
 
 	@Override
 	public List<Cell> getValidMoves(Cell[][] board) {
-		// TODO Auto-generated method stub
-		return null;
+		/**
+		 * Pawn Logic per Wikipedia:
+		 * 
+		 * A pawn can move forward to the unoccupied square immediately in front of it
+		 * on the same file, or on its first move it can advance two squares along the
+		 * same file, provided both squares are unoccupied (white dots in the diagram
+		 * above); or the pawn can capture an opponent's piece on a square diagonally in
+		 * front of it on an adjacent file, by moving to that square (black "getX()"s). When
+		 * a pawn makes a two-step advance from its starting position and there is an
+		 * opponent's pawn on a square next to the destination square on an adjacent
+		 * file, then the opponent's pawn can capture it en passant ("in passing"),
+		 * moving to the square the pawn passed over. This can be done only on the very
+		 * next turn; otherwise, the right to do so is forfeited. For example, in the
+		 * animated diagram, the black pawn advances two steps from g7 to g5, and the
+		 * white pawn on f5 can take it en passant on g6 (but only on White's next
+		 * move).
+		 * 
+		 */
+		
+		validMoves.clear();
+
+		// Step 1. Check to see if current piece is White
+		if (isWhite()) {
+
+			// Step 2. If our x value is 7, we cannot move any further, return
+			if (getX() == 7) {
+				return validMoves;
+			}
+			
+			// Step 3. If there is not a piece one space North, we can move forward
+			if (null == board[getX() + 1][getY()].getPiece()) {
+				validMoves.add(board[getX() + 1][getY()]);
+
+				// Step 4. If we are at our default starting position
+				if (getX() == 1) {
+					// Step 5. If there is not a piece two spaces in front, we can move.
+					// NOTE: This will only be done if the pawn has not moved
+					if (null == board[3][getY()].getPiece()) {
+						validMoves.add(board[3][getY()]);
+					}
+				}
+			}
+			
+			// Step 6. If we are not on the left corner
+			if (getY() > 0) {
+				// Step 7. Get the cell one space North and one space West
+				Cell c1 = board[getX() + 1][getY() - 1];
+				
+				// Step 8. If the space has a piece and that piece is not white, we can move and capture
+				if ((null != c1.getPiece()) && (this.isWhite() != c1.getPiece().isWhite())) {
+					validMoves.add(c1);
+				}
+			}
+
+			// Step 9. If we are not on the right corner
+			if (getY() < 7) {
+				// Step 10. Get the cell one space North and one space East
+				Cell c2 = board[getX() + 1][getY() + 1];
+
+				// Step 11. If the space has a piece and that piece is not white, we can move and capture
+				if ((null != c2.getPiece()) && (this.isWhite() != c2.getPiece().isWhite())) {
+					validMoves.add(c2);
+				}
+			}
+
+		} 
+		// Step 2. The current piece is black
+		else {
+			// Step 3. If our x value is 0, we cannot move any further, return
+			if (getX() == 0) {
+				return validMoves;
+			}
+
+			// Step 4. If there is not a piece one space South, we can move forward
+			if (null == board[getX() - 1][getY()].getPiece()) {
+				validMoves.add(board[getX() - 1][getY()]);
+
+				// Step 5. If we are at our default starting position
+				if (getX() == 6) {
+					// Step 6. If there is not a piece two spaces in front, we can move.
+					// NOTE: This will only be down if the pawn has not moved
+					if (null == board[4][getY()].getPiece()) {
+						validMoves.add(board[4][getY()]);
+					}
+				}
+			}
+
+			// Step 7. If we are not on the left corner
+			if (getY() > 0) {
+				// Step 8. Get the cell one space South and one space West
+				Cell c1 = board[getX() - 1][getY() - 1];
+				
+				// Step 9. If the space has a piece and that piece is not black, we can move and capture
+				if ((null != c1.getPiece()) && (this.isWhite() != c1.getPiece().isWhite())) {
+					validMoves.add(c1);
+				}
+			}
+
+			// Step 10. If we are not on the right corner
+			if (getY() < 7) {
+				// Step 11. Get the cell one space South and one space East
+				Cell c2 = board[getX() - 1][getY() + 1];
+				
+				// Step 12. If the space has a piece and that piece is not black, we can move and capture
+				if ((null != c2.getPiece()) && (this.isWhite() != c2.getPiece().isWhite())) {
+					validMoves.add(c2);
+				}
+			}
+		}
+
+		return validMoves;
 	}
 
 	@Override
 	public PieceEnum getPieceEnum() {
 		return PieceEnum.PAWN;
 	}
-
 }
--- src/test/java/com/thompson/apps/chess/pieces/TestPawn.java
+++ src/test/java/com/thompson/apps/chess/pieces/TestPawn.java
@@ -1,24 +1,628 @@
 package com.thompson.apps.chess.pieces;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import com.thompson.apps.chess.board.Cell;
+import com.thompson.apps.chess.board.ChessBoard;
+import com.thompson.apps.chess.board.Tile;
+
 public class TestPawn {
+	ChessBoard board = null;
+
+	AbstractPiece piece = null;
+
+	List<Cell> moves = null;
+
+	List<Tile> tileActualMoves = null;
+	List<Tile> tileExpectedMoves = null;
 
 	@Before
 	public void setUp() throws Exception {
+		board = new ChessBoard();
+		board.setDefaultBoard();
+
+		tileActualMoves = new ArrayList<Tile>();
+		tileExpectedMoves = new ArrayList<Tile>();
 	}
 
 	@After
 	public void tearDown() throws Exception {
+		board = null;
+
+		tileActualMoves = null;
+		tileExpectedMoves = null;
+	}
+
+	private void convertMovesToTiles(List<Cell> validMoves) {
+		boolean hasMoves = true;
+		String color = (piece.isWhite() ? "WHITE" : "BLACK");
+		System.out.println("LEGAL MOVES FOR " + color + " " + piece.toString() + " at (" + piece.getX() + ","
+				+ piece.getY() + "):");
+
+		if (null == validMoves || validMoves.isEmpty()) {
+			System.out.println("No legal moves!");
+			hasMoves = false;
+		} else {
+			for (Cell c : validMoves) {
+				for (Tile s : Tile.values()) {
+					if ((s.getX() == c.getX()) && (s.getY() == c.getY())) {
+						tileActualMoves.add(s);
+					}
+				}
+			}
+		}
+
+		if (hasMoves) {
+			System.out.println(tileActualMoves);
+		}
+
+		System.out.println();
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testDefaultPawn_WhitePieces() {
+		// First Pawn
+		piece = new Pawn(true, Tile.A2.getX(), Tile.A2.getY());
+		tileExpectedMoves.add(Tile.A3);
+		tileExpectedMoves.add(Tile.A4);
+
+		// Assert Moves
+		moves = piece.getValidMoves(board.getChessBoard());
+		System.out.println(board.printBoard());
+		convertMovesToTiles(moves);
+		assertEquals(tileExpectedMoves, tileActualMoves);
+
+		// Assert Piece Enum
+		assertEquals(PieceEnum.PAWN, piece.getPieceEnum());
+
+		// Assert Color
+		assertTrue(piece.isWhite());
+
+		// Assert toString
+		assertEquals("P", piece.toString());
+
+		// Second Pawn
+		piece = new Pawn(true, Tile.B2.getX(), Tile.B2.getY());
+		tileExpectedMoves.clear();
+		tileExpectedMoves.add(Tile.B3);
+		tileExpectedMoves.add(Tile.B4);
+
+		// Assert Moves
+		tileActualMoves.clear();
+		moves = piece.getValidMoves(board.getChessBoard());
+		System.out.println(board.printBoard());
+		convertMovesToTiles(moves);
+		assertEquals(tileExpectedMoves, tileActualMoves);
+
+		// Assert Piece Enum
+		assertEquals(PieceEnum.PAWN, piece.getPieceEnum());
+
+		// Assert Color
+		assertTrue(piece.isWhite());
+
+		// Assert toString
+		assertEquals("P", piece.toString());
+
+		// Third Pawn
+		piece = new Pawn(true, Tile.C2.getX(), Tile.C2.getY());
+		tileExpectedMoves.clear();
+		tileExpectedMoves.add(Tile.C3);
+		tileExpectedMoves.add(Tile.C4);
+
+		// Assert Moves
+		tileActualMoves.clear();
+		moves = piece.getValidMoves(board.getChessBoard());
+		System.out.println(board.printBoard());
+		convertMovesToTiles(moves);
+		assertEquals(tileExpectedMoves, tileActualMoves);
+
+		// Assert Piece Enum
+		assertEquals(PieceEnum.PAWN, piece.getPieceEnum());
+
+		// Assert Color
+		assertTrue(piece.isWhite());
+
+		// Assert toString
+		assertEquals("P", piece.toString());
+
+		// Fourth Pawn
+		piece = new Pawn(true, Tile.D2.getX(), Tile.D2.getY());
+		tileExpectedMoves.clear();
+		tileExpectedMoves.add(Tile.D3);
+		tileExpectedMoves.add(Tile.D4);
+
+		// Assert Moves
+		tileActualMoves.clear();
+		moves = piece.getValidMoves(board.getChessBoard());
+		System.out.println(board.printBoard());
+		convertMovesToTiles(moves);
+		assertEquals(tileExpectedMoves, tileActualMoves);
+
+		// Assert Piece Enum
+		assertEquals(PieceEnum.PAWN, piece.getPieceEnum());
+
+		// Assert Color
+		assertTrue(piece.isWhite());
+
+		// Assert toString
+		assertEquals("P", piece.toString());
+
+		// Fifth Pawn
+		piece = new Pawn(true, Tile.E2.getX(), Tile.E2.getY());
+		tileExpectedMoves.clear();
+		tileExpectedMoves.add(Tile.E3);
+		tileExpectedMoves.add(Tile.E4);
+
+		// Assert Moves
+		tileActualMoves.clear();
+		moves = piece.getValidMoves(board.getChessBoard());
+		System.out.println(board.printBoard());
+		convertMovesToTiles(moves);
+		assertEquals(tileExpectedMoves, tileActualMoves);
+
+		// Assert Piece Enum
+		assertEquals(PieceEnum.PAWN, piece.getPieceEnum());
+
+		// Assert Color
+		assertTrue(piece.isWhite());
+
+		// Assert toString
+		assertEquals("P", piece.toString());
+
+		// Sixth Pawn
+		piece = new Pawn(true, Tile.F2.getX(), Tile.F2.getY());
+		tileExpectedMoves.clear();
+		tileExpectedMoves.add(Tile.F3);
+		tileExpectedMoves.add(Tile.F4);
+
+		// Assert Moves
+		tileActualMoves.clear();
+		moves = piece.getValidMoves(board.getChessBoard());
+		System.out.println(board.printBoard());
+		convertMovesToTiles(moves);
+		assertEquals(tileExpectedMoves, tileActualMoves);
+
+		// Assert Piece Enum
+		assertEquals(PieceEnum.PAWN, piece.getPieceEnum());
+
+		// Assert Color
+		assertTrue(piece.isWhite());
+
+		// Assert toString
+		assertEquals("P", piece.toString());
+
+		// Seventh Pawn
+		piece = new Pawn(true, Tile.G2.getX(), Tile.G2.getY());
+		tileExpectedMoves.clear();
+		tileExpectedMoves.add(Tile.G3);
+		tileExpectedMoves.add(Tile.G4);
+
+		// Assert Moves
+		tileActualMoves.clear();
+		moves = piece.getValidMoves(board.getChessBoard());
+		System.out.println(board.printBoard());
+		convertMovesToTiles(moves);
+		assertEquals(tileExpectedMoves, tileActualMoves);
+
+		// Assert Piece Enum
+		assertEquals(PieceEnum.PAWN, piece.getPieceEnum());
+
+		// Assert Color
+		assertTrue(piece.isWhite());
+
+		// Assert toString
+		assertEquals("P", piece.toString());
+
+		// Eighth Pawn
+		piece = new Pawn(true, Tile.H2.getX(), Tile.H2.getY());
+		tileExpectedMoves.clear();
+		tileExpectedMoves.add(Tile.H3);
+		tileExpectedMoves.add(Tile.H4);
+
+		// Assert Moves
+		tileActualMoves.clear();
+		moves = piece.getValidMoves(board.getChessBoard());
+		System.out.println(board.printBoard());
+		convertMovesToTiles(moves);
+		assertEquals(tileExpectedMoves, tileActualMoves);
+
+		// Assert Piece Enum
+		assertEquals(PieceEnum.PAWN, piece.getPieceEnum());
+
+		// Assert Color
+		assertTrue(piece.isWhite());
+
+		// Assert toString
+		assertEquals("P", piece.toString());
 	}
 
+	@Test
+	public void testDefaultPawn_BlackPieces() {
+		// First Pawn
+		piece = new Pawn(false, Tile.A7.getX(), Tile.A7.getY());
+		tileExpectedMoves.add(Tile.A6);
+		tileExpectedMoves.add(Tile.A5);
+
+		// Assert Moves
+		moves = piece.getValidMoves(board.getChessBoard());
+		System.out.println(board.printBoard());
+		convertMovesToTiles(moves);
+		assertEquals(tileExpectedMoves, tileActualMoves);
+
+		// Assert Piece Enum
+		assertEquals(PieceEnum.PAWN, piece.getPieceEnum());
+
+		// Assert Color
+		assertFalse(piece.isWhite());
+
+		// Assert toString
+		assertEquals("p", piece.toString());
+
+		// Second Pawn
+		piece = new Pawn(false, Tile.B7.getX(), Tile.B7.getY());
+		tileExpectedMoves.clear();
+		tileExpectedMoves.add(Tile.B6);
+		tileExpectedMoves.add(Tile.B5);
+
+		// Assert Moves
+		tileActualMoves.clear();
+		moves = piece.getValidMoves(board.getChessBoard());
+		System.out.println(board.printBoard());
+		convertMovesToTiles(moves);
+		assertEquals(tileExpectedMoves, tileActualMoves);
+
+		// Assert Piece Enum
+		assertEquals(PieceEnum.PAWN, piece.getPieceEnum());
+
+		// Assert Color
+		assertFalse(piece.isWhite());
+
+		// Assert toString
+		assertEquals("p", piece.toString());
+
+		// Third Pawn
+		piece = new Pawn(false, Tile.C7.getX(), Tile.C7.getY());
+		tileExpectedMoves.clear();
+		tileExpectedMoves.add(Tile.C6);
+		tileExpectedMoves.add(Tile.C5);
+
+		// Assert Moves
+		tileActualMoves.clear();
+		moves = piece.getValidMoves(board.getChessBoard());
+		System.out.println(board.printBoard());
+		convertMovesToTiles(moves);
+		assertEquals(tileExpectedMoves, tileActualMoves);
+
+		// Assert Piece Enum
+		assertEquals(PieceEnum.PAWN, piece.getPieceEnum());
+
+		// Assert Color
+		assertFalse(piece.isWhite());
+
+		// Assert toString
+		assertEquals("p", piece.toString());
+
+		// Fourth Pawn
+		piece = new Pawn(false, Tile.D7.getX(), Tile.D7.getY());
+		tileExpectedMoves.clear();
+		tileExpectedMoves.add(Tile.D6);
+		tileExpectedMoves.add(Tile.D5);
+
+		// Assert Moves
+		tileActualMoves.clear();
+		moves = piece.getValidMoves(board.getChessBoard());
+		System.out.println(board.printBoard());
+		convertMovesToTiles(moves);
+		assertEquals(tileExpectedMoves, tileActualMoves);
+
+		// Assert Piece Enum
+		assertEquals(PieceEnum.PAWN, piece.getPieceEnum());
+
+		// Assert Color
+		assertFalse(piece.isWhite());
+
+		// Assert toString
+		assertEquals("p", piece.toString());
+
+		// Fifth Pawn
+		piece = new Pawn(false, Tile.E7.getX(), Tile.E7.getY());
+		tileExpectedMoves.clear();
+		tileExpectedMoves.add(Tile.E6);
+		tileExpectedMoves.add(Tile.E5);
+
+		// Assert Moves
+		tileActualMoves.clear();
+		moves = piece.getValidMoves(board.getChessBoard());
+		System.out.println(board.printBoard());
+		convertMovesToTiles(moves);
+		assertEquals(tileExpectedMoves, tileActualMoves);
+
+		// Assert Piece Enum
+		assertEquals(PieceEnum.PAWN, piece.getPieceEnum());
+
+		// Assert Color
+		assertFalse(piece.isWhite());
+
+		// Assert toString
+		assertEquals("p", piece.toString());
+
+		// Sixth Pawn
+		piece = new Pawn(false, Tile.F7.getX(), Tile.F7.getY());
+		tileExpectedMoves.clear();
+		tileExpectedMoves.add(Tile.F6);
+		tileExpectedMoves.add(Tile.F5);
+
+		// Assert Moves
+		tileActualMoves.clear();
+		moves = piece.getValidMoves(board.getChessBoard());
+		System.out.println(board.printBoard());
+		convertMovesToTiles(moves);
+		assertEquals(tileExpectedMoves, tileActualMoves);
+
+		// Assert Piece Enum
+		assertEquals(PieceEnum.PAWN, piece.getPieceEnum());
+
+		// Assert Color
+		assertFalse(piece.isWhite());
+
+		// Assert toString
+		assertEquals("p", piece.toString());
+
+		// Seventh Pawn
+		piece = new Pawn(false, Tile.G7.getX(), Tile.G7.getY());
+		tileExpectedMoves.clear();
+		tileExpectedMoves.add(Tile.G6);
+		tileExpectedMoves.add(Tile.G5);
+
+		// Assert Moves
+		tileActualMoves.clear();
+		moves = piece.getValidMoves(board.getChessBoard());
+		System.out.println(board.printBoard());
+		convertMovesToTiles(moves);
+		assertEquals(tileExpectedMoves, tileActualMoves);
+
+		// Assert Piece Enum
+		assertEquals(PieceEnum.PAWN, piece.getPieceEnum());
+
+		// Assert Color
+		assertFalse(piece.isWhite());
+
+		// Assert toString
+		assertEquals("p", piece.toString());
+
+		// Eighth Pawn
+		piece = new Pawn(false, Tile.H7.getX(), Tile.H7.getY());
+		tileExpectedMoves.clear();
+		tileExpectedMoves.add(Tile.H6);
+		tileExpectedMoves.add(Tile.H5);
+
+		// Assert Moves
+		tileActualMoves.clear();
+		moves = piece.getValidMoves(board.getChessBoard());
+		System.out.println(board.printBoard());
+		convertMovesToTiles(moves);
+		assertEquals(tileExpectedMoves, tileActualMoves);
+
+		// Assert Piece Enum
+		assertEquals(PieceEnum.PAWN, piece.getPieceEnum());
+
+		// Assert Color
+		assertFalse(piece.isWhite());
+
+		// Assert toString
+		assertEquals("p", piece.toString());
+	}
+
+	@Test
+	public void testCustomWhitePawn_NoMoves() {
+		// Create Blank Board
+		board = new ChessBoard();
+
+		// Create White Pawn at D4
+		piece = new Pawn(true, Tile.D8.getX(), Tile.D8.getY());
+		// Expected Moves
+
+		// Set Custom Pawn
+		board.setCustomPiece(piece);
+
+		// Assert Moves
+		moves = piece.getValidMoves(board.getChessBoard());
+		System.out.println(board.printBoard());
+		convertMovesToTiles(moves);
+		assertEquals(0, tileActualMoves.size());
+		assertTrue(tileActualMoves.isEmpty());
+	}
+
+	@Test
+	public void testCustomWhitePawn_WhitePieceInFront() {
+		// Create Blank Board
+		board = new ChessBoard();
+
+		// Create White Pawn at D4
+		piece = new Pawn(true, Tile.D4.getX(), Tile.D4.getY());
+		// Expected Moves
+
+		// Set Custom Pawn
+		board.setCustomPiece(piece);
+
+		// Create Pawn at D5
+		AbstractPiece p = new Pawn(true, Tile.D5.getX(), Tile.D5.getY());
+		board.setCustomPiece(p);
+
+		// Assert Moves
+		moves = piece.getValidMoves(board.getChessBoard());
+		System.out.println(board.printBoard());
+		convertMovesToTiles(moves);
+		assertEquals(0, tileActualMoves.size());
+		assertTrue(tileActualMoves.isEmpty());
+	}
+
+	@Test
+	public void testCustomWhitePawn_WhitePieceToLeftAndRight() {
+		// Create Blank Board
+		board = new ChessBoard();
+
+		// Create White Pawn at D4
+		piece = new Pawn(true, Tile.D4.getX(), Tile.D4.getY());
+		// Expected Moves
+		tileExpectedMoves.add(Tile.D5);
+
+		// Set Custom Pawn
+		board.setCustomPiece(piece);
+
+		// Create Pawn at C5
+		AbstractPiece p = new Pawn(true, Tile.C5.getX(), Tile.C5.getY());
+		board.setCustomPiece(p);
+
+		// Create Pawn at C5
+		p = new Pawn(true, Tile.E5.getX(), Tile.E5.getY());
+		board.setCustomPiece(p);
+
+		// Assert Moves
+		moves = piece.getValidMoves(board.getChessBoard());
+		System.out.println(board.printBoard());
+		convertMovesToTiles(moves);
+		assertEquals(tileActualMoves, tileActualMoves);
+	}
+
+	@Test
+	public void testCustomWhitePawn_BlackPieceToLeftAndRight() {
+		// Create Blank Board
+		board = new ChessBoard();
+
+		// Create White Pawn at D4
+		piece = new Pawn(true, Tile.D4.getX(), Tile.D4.getY());
+		// Expected Moves
+		tileExpectedMoves.add(Tile.D5);
+		tileExpectedMoves.add(Tile.C5);
+		tileExpectedMoves.add(Tile.E5);
+
+		// Set Custom Pawn
+		board.setCustomPiece(piece);
+
+		// Create Pawn at C5
+		AbstractPiece p = new Pawn(false, Tile.C5.getX(), Tile.C5.getY());
+		board.setCustomPiece(p);
+
+		// Create Pawn at C5
+		p = new Pawn(false, Tile.E5.getX(), Tile.E5.getY());
+		board.setCustomPiece(p);
+
+		// Assert Moves
+		moves = piece.getValidMoves(board.getChessBoard());
+		System.out.println(board.printBoard());
+		convertMovesToTiles(moves);
+		assertEquals(tileActualMoves, tileActualMoves);
+	}
+
+	@Test
+	public void testCustomBlackPawn_NoMoves() {
+		// Create Blank Board
+		board = new ChessBoard();
+
+		// Create Black Pawn at D4
+		piece = new Pawn(false, Tile.D1.getX(), Tile.D1.getY());
+		// Expected Moves
+
+		// Set Custom Pawn
+		board.setCustomPiece(piece);
+
+		// Assert Moves
+		moves = piece.getValidMoves(board.getChessBoard());
+		System.out.println(board.printBoard());
+		convertMovesToTiles(moves);
+		assertEquals(0, tileActualMoves.size());
+		assertTrue(tileActualMoves.isEmpty());
+	}
+
+	@Test
+	public void testCustomBlackPawn_BlackPieceInFront() {
+		// Create Blank Board
+		board = new ChessBoard();
+
+		// Create Black Pawn at D4
+		piece = new Pawn(false, Tile.D4.getX(), Tile.D4.getY());
+		// Expected Moves
+
+		// Set Custom Pawn
+		board.setCustomPiece(piece);
+
+		// Create Pawn at D5
+		AbstractPiece p = new Pawn(false, Tile.D3.getX(), Tile.D3.getY());
+		board.setCustomPiece(p);
+
+		// Assert Moves
+		moves = piece.getValidMoves(board.getChessBoard());
+		System.out.println(board.printBoard());
+		convertMovesToTiles(moves);
+		assertEquals(0, tileActualMoves.size());
+		assertTrue(tileActualMoves.isEmpty());
+	}
+
+	@Test
+	public void testCustomBlackPawn_BlackPieceToLeftAndRight() {
+		// Create Blank Board
+		board = new ChessBoard();
+
+		// Create Black Pawn at D4
+		piece = new Pawn(false, Tile.D4.getX(), Tile.D4.getY());
+		// Expected Moves
+		tileExpectedMoves.add(Tile.D3);
+
+		// Set Custom Pawn
+		board.setCustomPiece(piece);
+
+		// Create Pawn at C5
+		AbstractPiece p = new Pawn(false, Tile.C3.getX(), Tile.C3.getY());
+		board.setCustomPiece(p);
+
+		// Create Pawn at C5
+		p = new Pawn(false, Tile.E3.getX(), Tile.E3.getY());
+		board.setCustomPiece(p);
+
+		// Assert Moves
+		moves = piece.getValidMoves(board.getChessBoard());
+		System.out.println(board.printBoard());
+		convertMovesToTiles(moves);
+		assertEquals(tileActualMoves, tileActualMoves);
+	}
+
+	@Test
+	public void testCustomBlackPawn_WhitePieceToLeftAndRight() {
+		// Create Blank Board
+		board = new ChessBoard();
+
+		// Create Black Pawn at D4
+		piece = new Pawn(false, Tile.D4.getX(), Tile.D4.getY());
+		// Expected Moves
+		tileExpectedMoves.add(Tile.D3);
+		tileExpectedMoves.add(Tile.C3);
+		tileExpectedMoves.add(Tile.E3);
+
+		// Set Custom Pawn
+		board.setCustomPiece(piece);
+
+		// Create Pawn at C5
+		AbstractPiece p = new Pawn(true, Tile.C3.getX(), Tile.C3.getY());
+		board.setCustomPiece(p);
+
+		// Create Pawn at C5
+		p = new Pawn(true, Tile.E3.getX(), Tile.E3.getY());
+		board.setCustomPiece(p);
+
+		// Assert Moves
+		moves = piece.getValidMoves(board.getChessBoard());
+		System.out.println(board.printBoard());
+		convertMovesToTiles(moves);
+		assertEquals(tileActualMoves, tileActualMoves);
+	}
 }
